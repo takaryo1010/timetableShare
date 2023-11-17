c1164ac26e202d5b9f9ff9b4e9e83af835dc5043
fukuda_taichi <fukuda_taichi@room.rat.cis.k.hosei.ac.jp>
2023-11-17 17:44:41 +0900

    update calender: add login, signup, logout

--- article/myflask/app.py
+++ article/myflask/app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, redirect, url_for, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ca448a98'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
+@login_required
 def index():
     # ここで講義のデータを取得する
     lectures = {
@@ -20,15 +48,54 @@ def index():
     return render_template('lectures.html', timetable=lectures)
 
 
-@app.route("/home")
-def index_home():
-    return render_template('home.html')
-
-
-@app.route("/login")
-def index_login():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            flash(f'こんにちは、{username}さん。')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password.')
     return render_template('login.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user is None:
+            url = 'http://52.69.43.211/registerPerson'
+            data = {'name': username}  # 送信するデータをここで設定
+
+            response = requests.post(url, data=data)
+            json_response = response.json()
+            print(json_response)
+
+            if response.ok:
+                user = User()
+                user.username = username
+                user.set_password(password)
+                db.session.add(user)
+                db.session.commit()
+                flash('サインアップが正常に完了しました。')
+                return redirect(url_for('login'))
+            else:
+                flash('外部データベースへの登録に失敗しました。')
+        else:
+            flash('このユーザー名は既に存在します。')
+    return render_template('signup.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('ログアウトしました。')
+    return redirect(url_for('login'))
 
 @app.route("/timetable_registration")
 def index_timetable_registration():
@@ -83,4 +150,6 @@ def index_timetable_sharing():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
