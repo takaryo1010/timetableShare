2fad6b2d2858d25ed00614c805907c0332053bb4
fukuda_taichi <fukuda_taichi@room.rat.cis.k.hosei.ac.jp>
2023-11-25 23:02:46 +0900

    データベースの初期を app.py から切り離しました.

--- article/myflask/app.py
+++ article/myflask/app.py
@@ -209,6 +209,5 @@ def index_timetable_sharing():
 
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
     app.run(host='0.0.0.0', port='80', debug=True)
