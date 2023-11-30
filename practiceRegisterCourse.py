cac3e7028bc4f0a1149c3c396cb35ac9f0e173b5
unknown <taichita120@gmail.com>
2023-11-30 22:55:17 +0900

    テストプログラムを改善

--- practiceRegisterCourse.py
+++ practiceRegisterCourse.py
@@ -2,9 +2,10 @@ import requests
 
 # サーバーのエンドポイントURLを設定
 url = 'http://52.69.43.211/registerCource'  # サーバーの実際のURLに置き換えてください
-
+name = input("username>>")
+calssid = input("calssid>>")
 # 送信するデータを準備
-data = {'person_name': 'taichi', 'class_id' : 3}  # 送信するデータをここで設定
+data = {'person_name': name, 'class_id' : int(calssid)}  # 送信するデータをここで設定
 
 # HTTP POSTリクエストを送信
 response = requests.post(url, data=data)
