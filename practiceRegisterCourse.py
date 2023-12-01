e0c3da0fba5c6744399413a0fe5a801637b73452
takaryo1010 <t.ryota1010@gmail.com>
2023-12-01 14:55:02 +0900

    practiceRegisterCourse.py

--- practiceRegisterCourse.py
+++ practiceRegisterCourse.py
@@ -1,11 +1,11 @@
 import requests
 
 # サーバーのエンドポイントURLを設定
-url = 'http://52.69.43.211/registerCource'  # サーバーの実際のURLに置き換えてください
+url = 'http://52.69.43.211/registerCourse'  # サーバーの実際のURLに置き換えてください
 name = input("username>>")
 classid = input("classid>>")
 # 送信するデータを準備
-data = {'person_name': name, 'class_id' : classid}  # 送信するデータをここで設定
+data = {'person_name': name, 'class_id': classid}  # 送信するデータをここで設定
 
 # HTTP POSTリクエストを送信
 response = requests.post(url, data)
