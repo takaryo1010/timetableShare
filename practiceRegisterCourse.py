f384b91296d10e461f959e60de5e4b62c39858c0
keyta179 <keita.nozawa802@gmail.com>
2023-12-01 14:15:06 +0900

    fix practiceRegisterCourse.py

--- practiceRegisterCourse.py
+++ practiceRegisterCourse.py
@@ -3,7 +3,7 @@ import requests
 # サーバーのエンドポイントURLを設定
 url = 'http://52.69.43.211/registerCource'  # サーバーの実際のURLに置き換えてください
 name = input("username>>")
-classid = input("calssid>>")
+classid = input("classid>>")
 # 送信するデータを準備
 data = {'person_name': name, 'class_id' : int(classid)}  # 送信するデータをここで設定
 
