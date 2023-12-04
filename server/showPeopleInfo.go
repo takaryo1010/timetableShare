35b46061c2bc8cc4f9c7e4d5ddd23813c527f46b
keyta179 <keita.nozawa802@gmail.com>
2023-12-04 14:15:28 +0900

    add showPeopleInfo.go

--- server/showPeopleInfo.go
+++ server/showPeopleInfo.go
@@ -68,3 +68,29 @@ func showPeopleInfoAll(c echo.Context) error {
 
 	return c.JSON(http.StatusCreated, personInfos)
 }
+
+func showPeopleTakingSpecificClasses(c echo.Context) error {
+	// 入力データの解析
+	class_id := c.FormValue("class_id")
+
+	// データベースのハンドルを取得する
+	db, err := sql.Open("mysql", db_state)
+	if err != nil {
+		log.Fatal(err)
+		return err // エラーを返す
+	}
+	defer db.Close()
+
+	// SQLクエリの構築（名前を条件に追加）
+	query1 := "SELECT p.name FROM Person p JOIN Course c ON p.id = c.person_id WHERE c.class_id = ?"
+
+	// SQLの準備
+	var name string
+	err = db.QueryRow(query1, class_id).Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+		return c.JSON(http.StatusCreated, err) // エラーを返す
+	}
+
+	return c.JSON(http.StatusOK, name)
+}
