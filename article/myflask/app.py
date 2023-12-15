2ad80f26b4811156bd8b449bf6890a76d98bf2ba
keyta179 <keita.nozawa802@gmail.com>
2023-12-15 14:53:42 +0900

    fix timetable_sharing.html

--- article/myflask/app.py
+++ article/myflask/app.py
@@ -261,89 +261,89 @@ def index_lecture_creation():
 @login_required
 def share_index():
     
-    # # サーバーのエンドポイントURLを設定
+    # サーバーのエンドポイントURLを設定
     
-    # data = {'my_name':current_user.username}
-    # friends = []
-    # friendsTimetables = {}
-    # response = requests.post('http://52.69.43.211/showFriends', data=data)
-    # json_response = response.json()
-    # print(json_response)
-    # friends.append(current_user.username)
-    # for y in json_response:
-    #     friends.append( y['your_name'])
+    data = {'my_name':current_user.username}
+    friends = []
+    friendsTimetables = {}
+    response = requests.post('http://52.69.43.211/showFriends', data=data)
+    json_response = response.json()
+    print(json_response)
+    friends.append(current_user.username)
+    for y in json_response:
+        friends.append( y['your_name'])
         
-    # for y in friends:
-    # # 送信するデータを準備
-    #     data = {'name': y}  # 送信するデータをここで設定
-    #     print(data)
+    for y in friends:
+    # 送信するデータを準備
+        data = {'name': y}  # 送信するデータをここで設定
+        print(data)
 
         
-    # # HTTP POSTリクエストを送信
+    # HTTP POSTリクエストを送信
        
-    #     response = requests.post('http://52.69.43.211/showMyClassInfo', data=data)
+        response = requests.post('http://52.69.43.211/showMyClassInfo', data=data)
 
-    # # レスポンスをJSONとしてパース
-    #     json_response = response.json()
-    #     print(json_response)
-    #     monday = ["", "", "", "", ""]
-    #     tuesday = ["", "", "", "", ""]
-    #     wednesday = ["", "", "", "", ""]
-    #     thursday = ["", "", "", "", ""]
-    #     friday = ["", "", "", "", ""]
-    #     saturday = ["", "", "", "", ""]
-    #     ondemand = ["", "", "", "", ""]
-    #     for class_ in json_response:
-    #         if class_['day'] == 'Monday':
-    #             monday[int(class_['period']) - 1]  = class_['name']
-    #         elif class_['day'] == 'Tuesday':
-    #             tuesday[int(class_['period']) - 1] = class_['name']
-    #         elif class_['day'] == 'Wednesday':
-    #             wednesday[int(class_['period']) - 1] = class_['name']
-    #         elif class_['day'] == 'Thursday':
-    #             thursday[int(class_['period']) - 1] = class_['name']
-    #         elif class_['day'] == 'Friday':
-    #             friday[int(class_['period']) - 1] = class_['name']
-    #         elif class_['day'] == 'Saturday':
-    #             saturday[int(class_['period']) - 1] = class_['name']
-    #         elif class_['day'] == 'Ondemand':
-    #             ondemand[int(class_['period']) - 1] = class_['name']
+    # レスポンスをJSONとしてパース
+        json_response = response.json()
+        print(json_response)
+        monday = ["", "", "", "", ""]
+        tuesday = ["", "", "", "", ""]
+        wednesday = ["", "", "", "", ""]
+        thursday = ["", "", "", "", ""]
+        friday = ["", "", "", "", ""]
+        saturday = ["", "", "", "", ""]
+        ondemand = ["", "", "", "", ""]
+        for class_ in json_response:
+            if class_['day'] == 'Monday':
+                monday[int(class_['period']) - 1]  = class_['name']
+            elif class_['day'] == 'Tuesday':
+                tuesday[int(class_['period']) - 1] = class_['name']
+            elif class_['day'] == 'Wednesday':
+                wednesday[int(class_['period']) - 1] = class_['name']
+            elif class_['day'] == 'Thursday':
+                thursday[int(class_['period']) - 1] = class_['name']
+            elif class_['day'] == 'Friday':
+                friday[int(class_['period']) - 1] = class_['name']
+            elif class_['day'] == 'Saturday':
+                saturday[int(class_['period']) - 1] = class_['name']
+            elif class_['day'] == 'Ondemand':
+                ondemand[int(class_['period']) - 1] = class_['name']
 
-    #     lectures = {
-    #         "Monday": monday,
-    #         "Tuesday": tuesday,
-    #         "Wednesday": wednesday,
-    #         "Thursday": thursday,
-    #         "Friday": friday,
-    #         "Saturday": saturday,
-    #         "Ondemand": ondemand
-    #     }
-    #     print (lectures)
-    #     friendsTimetables[y]=lectures
-    
-    friendsTimetablestest = {}
-    friends_list = ['友達1', '友達2', '友達3']  # 仮の友達リスト（実際のデータに置き換える必要があります）
-    monday = ["", "", "", "", ""]
-    tuesday = ["", "", "", "", ""]
-    wednesday = ["", "", "", "", ""]
-    thursday = ["", "", "", "", ""]
-    friday = ["", "", "", "", ""]
-    saturday = ["", "", "", "", ""]
-    ondemand = ["", "", "", "", ""]
-    for y in friends_list:
         lectures = {
-                "Monday": monday,
-                "Tuesday": tuesday,
-                "Wednesday": wednesday,
-                "Thursday": thursday,
-                "Friday": friday,
-                "Saturday": saturday,
-                "Ondemand": ondemand
-            }
-        friendsTimetablestest[y]=lectures
+            "Monday": monday,
+            "Tuesday": tuesday,
+            "Wednesday": wednesday,
+            "Thursday": thursday,
+            "Friday": friday,
+            "Saturday": saturday,
+            "Ondemand": ondemand
+        }
+        print (lectures)
+        friendsTimetables[y]=lectures
     
-    return render_template('timetable_sharing.html', friendstimetable=friendsTimetablestest)
-    # return render_template('timetable_sharing.html', friendstimetable=friendsTimetables)
+    # friendsTimetablestest = {}
+    # friends_list = ['友達1', '友達2', '友達3']  # 仮の友達リスト（実際のデータに置き換える必要があります）
+    # monday = ["", "", "", "", ""]
+    # tuesday = ["", "", "", "", ""]
+    # wednesday = ["", "", "", "", ""]
+    # thursday = ["", "", "", "", ""]
+    # friday = ["", "", "", "", ""]
+    # saturday = ["", "", "", "", ""]
+    # ondemand = ["", "", "", "", ""]
+    # for y in friends_list:
+    #     lectures = {
+    #             "Monday": monday,
+    #             "Tuesday": tuesday,
+    #             "Wednesday": wednesday,
+    #             "Thursday": thursday,
+    #             "Friday": friday,
+    #             "Saturday": saturday,
+    #             "Ondemand": ondemand
+    #         }
+    #     friendsTimetablestest[y]=lectures
+    
+    # return render_template('timetable_sharing.html', friendstimetable=friendsTimetablestest)
+    return render_template('timetable_sharing.html', friendstimetable=friendsTimetables)
 
 
 
