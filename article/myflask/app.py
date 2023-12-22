1f70de2c19f8981dde00466d7f282e914a9147ef
fukuda_taichi <fukuda_taichi@room.rat.cis.k.hosei.ac.jp>
2023-12-22 14:08:19 +0900

    registration エラー修正

--- article/myflask/app.py
+++ article/myflask/app.py
@@ -201,8 +201,8 @@ def index_timetable_registration():
             url = 'http://52.69.43.211/showClassInfoTimeSpecification'
             response = requests.post(url, data=data)
             json_response = response.json()
-            print(data['name'])
             print(json_response)
+            print(request.form.get('class_name'))
             return render_template('timetable_registration.html', data=data, json=json_response)
         else:
             url = 'http://52.69.43.211/registerCourse'
